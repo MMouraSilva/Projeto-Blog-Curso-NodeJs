3c43b4a9a0efaf36fe15a47a97b578ef987bcb66
Matheus Moura <matheusmoura78@hotmail.com>
2023-11-22 23:57:42 -0300

    Criação da página de artigos

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("./Article");
+const Category = require("../categories/Category");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-    res.send("ROTA DE ARTIGOS");
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles });
+    });
 });
 
-router.get("admin/articles/new", (req, res) => {
-    res.send("ROTA PARA CRIAR UMA NOVA ARTIGOS");
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", { categories });
+    })
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title,
+        slug: slugify(title),
+        body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+
+    if(id && !isNaN(id)) {
+        Article.destroy({
+            where: {
+                id
+            }
+        }).then(() => {
+            res.redirect("/admin/articles");
+        });
+    } else {
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("GUIAPRESS", "sa", "123456", {
     host: "localhost",
-    dialect: "mssql"
+    dialect: "mssql",
+    timezone: "-03:00"
 });
 
 module.exports = connection;
